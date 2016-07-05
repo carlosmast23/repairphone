ec7e07f83e14d532898506d0b23a38a07d2b79c1
carlosmast23 <carlosmast2301@hotmail.es>
2016-07-05 06:48:11 -0500

    Merge origin/master
    Conflicts:
    	codefac-web/src/main/java/ec/com/codesoft/web/operador/GestionProductoMB.java
    	codefac-web/src/main/java/ec/com/codesoft/web/operador/comprarMB.java

---
+++ core/InterfazDatos.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author home
+ */
+interface InterfazDatos {
+    public function getTabla();
+    public function getDatos();
+}
+
+?>
